3eda62039ed0dfdd6428c66e2ed442e7fa6804e8
adam <twoj@example.com>
2024-04-26 18:42:36 +0200

    Complete connecting portfolio html file to flask

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+@app.route('/')
+def home():
+    return  render_template('index.html')
+
+@app.route('/portfolio')
+def test():
+    return render_template('portfolio.html')
+
+if __name__ == '__main__':
+    app.run()
